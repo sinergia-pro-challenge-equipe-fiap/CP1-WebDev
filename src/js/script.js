a52cc8a5bae3e2807a0d63aed19309bea2da59eb
Ronaldo <rronaldo.monteiro.almeida@gmail.com>
2025-04-17 14:21:55 -0300

    criando o ex1: Criando variaveis com valor, indefinida e vazia

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
-//teste
+// EXERCÍCIO 1
+
+// Criando uma variável com valor
+let variavel1 = 10;
+
+// Criando uma variável indefinida
+let variavel2;
+
+// Criando uma variável vazia
+let variavel3 = null;
+
+//Imprimindo as variáveis no console
+console.log(variavel1);
+console.log(variavel2);
+console.log(variavel3);
